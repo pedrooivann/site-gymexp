c21e0be75c78aff1abafa04f2fa2f03c1344d5c9
Ivan Pedro dos S. Pinheiro <ivanpsan.san@gmail.com>
2025-10-17 20:36:27 -0300

    Adicionado Botão para remover Linha

--- planilhadetreino.js
+++ planilhadetreino.js
@@ -11,17 +11,21 @@ botaofuncao.forEach((botao) => {
         const novo_exercicio = document.createElement("td");
         const nova_serie = document.createElement("td");
         const nova_repeticao = document.createElement("td");
+        const novo_removerbtn = document.createElement("td");
 
         //Cria os inputs
         const input_exercicio = document.createElement("input");
         const input_serie = document.createElement("input");
         const input_repeticao = document.createElement("input");
 
+        //Cria botão de remover
+        const removerLinhaBtn = document.createElement("button");
 
         input_exercicio.type = "text";
         input_serie.type = "number";
         input_repeticao.type = "number";
 
+        //Estilização dos Inputs criados
         Object.assign(input_exercicio.style, {
             width: "200px",
             textAlign: "left",
@@ -49,27 +53,29 @@ botaofuncao.forEach((botao) => {
             })
         })
 
-        //Inputs
-
-        /*
-        novo_exercicio.className = "tinput_exercicio";
-        nova_serie.className = "tinput_serie";
-        nova_repeticao.className = "tinuput_repeticao";
-        */
-
+        //Estilização botão de remover linha
+        removerLinhaBtn.textContent = "❌";
+        removerLinhaBtn.style.cursor = "pointer";
+        removerLinhaBtn.style.backgroundColor = "none";
+        removerLinhaBtn.style.border = "none";
 
         //Coloca os inputs dentro das células
         novo_exercicio.appendChild(input_exercicio);
         nova_serie.appendChild(input_serie);
         nova_repeticao.appendChild(input_repeticao);
-
+        novo_removerbtn.appendChild(removerLinhaBtn);
 
         //Coloca as células dentro da linha
         linha.appendChild(novo_exercicio);
         linha.appendChild(nova_serie);
         linha.appendChild(nova_repeticao);
+        linha.appendChild(novo_removerbtn);
 
         plan_tabela.appendChild(linha);
+
+        removerLinhaBtn.addEventListener('click', () => {
+            linha.remove();
+        })
 
     })
 });
